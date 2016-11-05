a43ed66e656d5bb0c3f2726a59c57ea175d5d255
smerdy <joshua.pham@gmail.com>
2016-11-05 16:42:14 -0500

    finished adding periods to all songs

--- converttxt.py
+++ converttxt.py
@@ -1,14 +1,22 @@
 import os
+import string
 
 basepath = './'
-for fname in os.listdir(basepath):
+for fname in os.listdir(basepath + '/txt/'):
     path = os.path.join(basepath, fname)
     if not os.path.isdir(path):
         filewhole = path.split('.')
         filename = filewhole[1][1:]
         ext = filewhole[-1]
         if ext == 'txt':
-            infile = open(filename + '.' + ext, 'r')
-            outfile = open(filename + '_dot.' + ext, 'w')
+            infile = open('./txt/' + filename + '.' + ext, 'r')
+            outfile = open('./txt_period/' + filename + '.' + ext, 'w')
+            infile.readline()
+            infile.readline()
+            infile.readline()
             for line in infile:
-                outfile.write(line[:-1] + '.\n')
+                line = string.strip(line)
+                if line:
+                    outfile.write(line + '.\n')
+                else:
+                    outfile.write('\n')
