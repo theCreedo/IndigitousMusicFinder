2903a97b28f8b194695faf75dc0e212e2918f80d
theCreedo <hownl96@gmail.com>
2016-11-06 02:14:16 -0600

    Refactored files and added comments

--- songs_to_moods/show_data.py
+++ songs_to_moods/show_data.py
@@ -11,6 +11,7 @@ mySadness = {}
 
 basepath = './'
 # Processes all files in given path
+# EDIT FILEPATH BASED ON BASEPATH
 for fname in os.listdir(basepath + '/All/'):
     path = os.path.join(basepath, fname)
     # Enter if not directory (is file)
@@ -26,44 +27,64 @@ for fname in os.listdir(basepath + '/All/'):
             count = 0
             name = ''
             # Get all data from file
+            # EDIT FILEPATH BASED ON BASEPATH
             with open('./All/' + filename + '.txt', 'r') as data_file:
                 for line in data_file:
+                    # Save name of file
                     if count == 0:
                         name = line
+                    # Add filename and max value to Anger dictionary
                     if count == 3:
                         word = line.split(' ')[1]
                         myAnger[name] = float(word[:len(word)-1])
+                    # Add filename and max value to Disgust dictionary
                     if count == 4:
                         word = line.split(' ')[1]
                         myDisgust[name] = float(word[:len(word)-1])
+                    # Add filename and max value to Fear dictionary
                     if count == 5:
                         word = line.split(' ')[1]
                         myFear[name] = float(word[:len(word)-1])
+                    # Add filename and max value to Joy dictionary
                     if count == 6:
                         word = line.split(' ')[1]
                         myJoy[name] = float(word[:len(word)-1])
+                    # Add filename and max value to Sadness dictionary
                     if count == 7:
                         word = line.split(' ')[1]
                         mySadness[name] = float(word[:len(word)-1])
                     count +=1
-
-outfile = open('./Anger/sorted_low_to_high.txt', 'w')
+#Populate anger_low_to_hight.txt file with dictionary files sorted from low to high
+# EDIT FILEPATH BASED ON BASEPATH
+outfile = open('./Sorted_Tones/anger_low_to_high.txt', 'w')
 for key, value in sorted(myAnger.iteritems(), key=lambda (k,v): (v,k)):
     outfile.write(key + str(value) + '\n')
 outfile.close()
-outfile = open('./Disgust/sorted_low_to_high.txt', 'w')
+
+#Populate disgust_low_to_high.txt file with dictionary files sorted from low to high
+# EDIT FILEPATH BASED ON BASEPATH
+outfile = open('./Sorted_Tones/disgust_low_to_high.txt', 'w')
 for key, value in sorted(myDisgust.iteritems(), key=lambda (k,v): (v,k)):
     outfile.write(key + str(value) + '\n')
 outfile.close()
-outfile = open('./Fear/sorted_low_to_high.txt', 'w')
+
+#Populate fear_low_to_high.txt file with dictionary files sorted from low to high
+# EDIT FILEPATH BASED ON BASEPATH
+outfile = open('./Sorted_Tones/fear_low_to_high.txt', 'w')
 for key, value in sorted(myFear.iteritems(), key=lambda (k,v): (v,k)):
     outfile.write(key + str(value) + '\n')
 outfile.close()
-outfile = open('./Joy/sorted_low_to_high.txt', 'w')
+
+#Populate joy_low_to_high.txt file with dictionary files sorted from low to high
+# EDIT FILEPATH BASED ON BASEPATH
+outfile = open('./Sorted_Tones/joy_low_to_high.txt', 'w')
 for key, value in sorted(myJoy.iteritems(), key=lambda (k,v): (v,k)):
     outfile.write(key + str(value) + '\n')
 outfile.close()
-outfile = open('./Sadness/sorted_low_to_high.txt', 'w')
+
+#Populate sadness_low_to_hight.txt file with dictionary files sorted from low to high
+# EDIT FILEPATH BASED ON BASEPATH
+outfile = open('./Sorted_Tones/sadness_low_to_high.txt', 'w')
 for key, value in sorted(mySadness.iteritems(), key=lambda (k,v): (v,k)):
     outfile.write(key + str(value) + '\n')
 outfile.close()
