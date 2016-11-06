3dbc542434e27f52e940688b9d90fd61c14cfc89
smerdy <joshua.pham@gmail.com>
2016-11-05 20:41:35 -0500

    successfully executed watson requests for all songs and have json files

--- tone_analyzer.py
+++ tone_analyzer.py
@@ -3,7 +3,22 @@ import codecs
 import json
 from watson_developer_cloud import ToneAnalyzerV3
 
-tone_analyzer = ToneAnalyzerV3(username='', password='', version='2016-05-19')
+tone_analyzer = ToneAnalyzerV3(username='98a2f9b7-e29f-4cd8-a6a0-a53073de93ea', password='bTsfFVjXlVSr', version='2016-05-19')
+
+
+def tryWatson(data, outfile, numtries):
+    try:
+        response = tone_analyzer.tone(text=data)
+        outfile.write(json.dumps(response))
+        print 'succeeded one request\n'
+    except Exception:
+        print 'Hit error in Watson ' + str(numtries) + ' times\n'
+        if numtries < 3:
+            tryWatson(data, outfile, numtries + 1)
+    pass
+
+
+
 
 basepath = './'
 # Processes all files in given path
@@ -23,6 +38,6 @@ for fname in os.listdir(basepath + '/txt_period/'):
             # Loop through all the lines in the file
             with codecs.open('./txt_period/' + filename + '.' + ext, 'r', encoding='utf8') as fin:
                 data = fin.read()
-            # Write complete json data to the output file
-            outfile.write(json.dumps(tone_analyzer.tone(text=data)))
+            tryWatson(data, outfile, 0)
             outfile.close()
+
