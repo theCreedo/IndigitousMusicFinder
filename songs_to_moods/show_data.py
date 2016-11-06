26cc44d25d1952a6a6115a32299663b683987afd
theCreedo <hownl96@gmail.com>
2016-11-06 02:04:09 -0600

    Added low to high files for all tones. Refactored .py names

--- songs_to_moods/show_data.py
+++ songs_to_moods/show_data.py
@@ -2,14 +2,16 @@ import os
 import json
 
 mood = {'Anger':0, 'Disgust':1, 'Fear':2, 'Joy':3, 'Sadness':4}
+# Create dictionaries for all files
 myAnger = {}
 myDisgust = {}
 myFear = {}
 myJoy = {}
 mySadness = {}
+
 basepath = './'
 # Processes all files in given path
-for fname in os.listdir(basepath + '/songs_to_moods/All/'):
+for fname in os.listdir(basepath + '/All/'):
     path = os.path.join(basepath, fname)
     # Enter if not directory (is file)
     if not os.path.isdir(path):
@@ -20,12 +22,11 @@ for fname in os.listdir(basepath + '/songs_to_moods/All/'):
         # Execute if .json file
         if ext == 'txt':
             # Create out .json files to write to
-            # outfile = open('./songs_to_moods/' + filename + '.txt', 'w')
             data = ''
             count = 0
-            # Loop through all the lines in the file
             name = ''
-            with open('./songs_to_moods/All/' + filename + '.txt', 'r') as data_file:
+            # Get all data from file
+            with open('./All/' + filename + '.txt', 'r') as data_file:
                 for line in data_file:
                     if count == 0:
                         name = line
@@ -33,29 +34,36 @@ for fname in os.listdir(basepath + '/songs_to_moods/All/'):
                         word = line.split(' ')[1]
                         myAnger[name] = float(word[:len(word)-1])
                     if count == 4:
+                        word = line.split(' ')[1]
                         myDisgust[name] = float(word[:len(word)-1])
                     if count == 5:
+                        word = line.split(' ')[1]
                         myFear[name] = float(word[:len(word)-1])
                     if count == 6:
+                        word = line.split(' ')[1]
                         myJoy[name] = float(word[:len(word)-1])
                     if count == 7:
+                        word = line.split(' ')[1]
                         mySadness[name] = float(word[:len(word)-1])
                     count +=1
 
-print 'Anger'
+outfile = open('./Anger/sorted_low_to_high.txt', 'w')
 for key, value in sorted(myAnger.iteritems(), key=lambda (k,v): (v,k)):
-    print "%s%s" % (key, value)
-print '\nDisgust'
+    outfile.write(key + str(value) + '\n')
+outfile.close()
+outfile = open('./Disgust/sorted_low_to_high.txt', 'w')
 for key, value in sorted(myDisgust.iteritems(), key=lambda (k,v): (v,k)):
-    print "%s%s" % (key, value)
-print '\nFear'
+    outfile.write(key + str(value) + '\n')
+outfile.close()
+outfile = open('./Fear/sorted_low_to_high.txt', 'w')
 for key, value in sorted(myFear.iteritems(), key=lambda (k,v): (v,k)):
-    print "%s%s" % (key, value)
-print '\nJoy'
+    outfile.write(key + str(value) + '\n')
+outfile.close()
+outfile = open('./Joy/sorted_low_to_high.txt', 'w')
 for key, value in sorted(myJoy.iteritems(), key=lambda (k,v): (v,k)):
-    print "%s%s" % (key, value)
-print '\nSadness'
+    outfile.write(key + str(value) + '\n')
+outfile.close()
+outfile = open('./Sadness/sorted_low_to_high.txt', 'w')
 for key, value in sorted(mySadness.iteritems(), key=lambda (k,v): (v,k)):
-    print "%s%s" % (key, value)
-            # outfile.write(max_sentence + '\n' + tone_word_sentence + '\n' + str(round(max_tone_sentence * 100)))
-            # outfile.close()
+    outfile.write(key + str(value) + '\n')
+outfile.close()
