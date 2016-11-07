574060b8b3dff16d5e0e2950d6292807d3c75072
smerdy <joshua.pham@gmail.com>
2016-11-07 13:52:35 -0600

    FINISHED for presentation @ #Hack Austin: visualization of mood progression by hosting webpage through simple Flask server

--- server/app.py
+++ server/app.py
@@ -7,14 +7,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	binned_songs = pickle.load(open('../topic_groupings/Anger.p', 'rb'))
+	binned_songs = pickle.load(open('../topic_groupings/Joy.p', 'rb'))
 	song_list = []
 	# for key, val in binned_songs.items():
 	# 	for song in val:
 	# 		with open('../txt/' + song['title'] + '.txt') as lyric_file:
 	# 			song_list.append(lyric_file.read())
 	for key, val in binned_songs.items():
-		for song in val:
+		for song in sorted(val, reverse=False, key=lambda x: x['score']):
 			song_list.append((song['saying'], url_for('static', filename=song['title'] + '.png')))
+		break
 	return render_template('display_lyrics.html', songs=song_list)
 
