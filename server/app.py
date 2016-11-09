ac3050aa3a6d6cc375d4a285d0a0dde78e8f93f8
smerdy <joshua.pham@gmail.com>
2016-11-09 16:13:16 -0600

    fixed frontend layout, display context of saying, censor profanity

--- server/app.py
+++ server/app.py
@@ -2,20 +2,43 @@ import os
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 import pickle
+from collections import deque
+import re
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	binned_songs = pickle.load(open('../topic_groupings/Joy.p', 'rb'))
-	song_list = []
-	# for key, val in binned_songs.items():
-	# 	for song in val:
-	# 		with open('../txt/' + song['title'] + '.txt') as lyric_file:
-	# 			song_list.append(lyric_file.read())
+
+	# create a regex string out of an external file with profanity words
+	censored_regex = '|'.join(open('censored_profanity.txt').read().split('\n'))
+
+	# list of lyrics snippets from all songs.
+	saying_context_list = []
 	for key, val in binned_songs.items():
 		for song in sorted(val, reverse=False, key=lambda x: x['score']):
-			song_list.append((song['saying'], url_for('static', filename=song['title'] + '.png')))
-		break
-	return render_template('display_lyrics.html', songs=song_list)
+
+			# get the lines surrounding the saying
+			with open('../txt/' + song['title'] + '.txt') as lyric_file:
+				lyrics_lines = lyric_file.read().split('\n')
+
+				five_lines = deque(lyrics_lines[:5])
+				for line in lyrics_lines[5:]:
+					if not line or line == '\n':
+						continue
+					if song['saying'] in five_lines[2]:
+						break
+
+					# replace all occurrences of profanity
+					line = re.sub(censored_regex, '*****', line)
+					
+					five_lines.popleft()
+					five_lines.append(line)
+
+				saying_context_list.append((five_lines, url_for('static', filename=song['title'] + '.png')))
+
+		break # break here, meaning we only display the songs for the first topic.
+
+	return render_template('display_lyrics.html', songs=saying_context_list)
 
